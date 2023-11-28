aa9e213f9c2620ff449537caba889b3862d78bcd
Adam Bechtold <adamcbechtold@gmail.com>
2023-11-28 12:37:02 -0500

    so we have a simple express and typescript server
    Guided by https://auth0.com/blog/node-js-and-typescript-tutorial-build-a-crud-api/

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("hello world, here's an update");
+/**
+ * Required External Modules
+ */
+
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+/**
+ * App Variables
+ */
+
+if (!process.env.PORT) {
+  console.error("Set a PORT value in the .env file.");
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+/**
+ * App Configuration
+ */
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+/**
+ * Server Activation
+ */
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
