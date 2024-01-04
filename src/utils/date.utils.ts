e1cc63e47f8061bed4364d3753665817c519cb48
Adam Bechtold <adamcbechtold@gmail.com>
2024-01-04 15:55:06 -0500

    so we display a simple play state interface with static values

--- src/utils/date.utils.ts
+++
@@ -1,7 +0,0 @@
-export function dateToUnixTimestamp(date: Date): number {
-  return Math.floor(date.getTime() / 1000);
-}
-
-export function unixTimestampToDate(unixTimestamp: number): Date {
-  return new Date(unixTimestamp * 1000);
-}
