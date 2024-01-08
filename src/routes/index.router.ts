95f59b43dae92c4883fe4ee4c7f96fe4243e3250
Adam Bechtold <adamcbechtold@gmail.com>
2024-01-08 17:28:12 -0500

    so users can choose any lastfm user we've pre-analyzed

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -1,15 +1,91 @@
 import express, { Request, Response } from "express";
 import { getCurrentUser } from "../auth/auth.utils";
+
+import * as UserService from "../users/users.service";
 import * as SpotifyService from "../spotify/spotify.service";
+
 import { SpotifyAccessToken } from "../auth/auth.types";
 import { secondsToTimeFormat } from "../utils/datetime.utils";
+import { NotFoundError, TypedError } from "../errors/errors.types";
+import { handleErrorResponse } from "../utils/response.utils";
+import { UserWithLastfmAccountAndId } from "../users/users.types";
 
 export const indexRouter = express.Router();
 
 indexRouter.get("/", async (req: Request, res: Response) => {
-  res.render("index", {
-    secondsToTimeFormat: secondsToTimeFormat,
+  const user1Username = req.query.user1 as string;
+  const user2Username = req.query.user2 as string;
+
+  if (user1Username || user2Username) {
+    res.redirect(
+      `/taste-comparison?user1=${user1Username}&user2=${user2Username}`
+    );
+    return;
+  }
+
+  const users = await UserService.getAllUsers();
+  const usersWithLastfmAccounts = users.filter(
+    (user) => !!user.lastfmAccount && !!user.lastfmAccount.username
+  ) as UserWithLastfmAccountAndId[];
+
+  res.render("configure-comparison", {
+    users: sortUsersByLastfmUsername(usersWithLastfmAccounts),
   });
+});
+
+indexRouter.get("/taste-comparison", async (req: Request, res: Response) => {
+  const user1Username = req.query.user1 as string;
+  const user2Username = req.query.user2 as string;
+
+  // check if the users are valid
+  let user1: UserWithLastfmAccountAndId | null = null;
+  let user2: UserWithLastfmAccountAndId | null = null;
+
+  try {
+    user1 = await UserService.getUsersByLastfmUsername(user1Username);
+  } catch (e: any) {
+    if (e instanceof TypedError && e instanceof NotFoundError) {
+      // Continue. Let's check user2.
+    } else {
+      handleErrorResponse(e, res);
+    }
+  }
+
+  try {
+    if (!user2Username) {
+      throw TypedError.create("User 2 not found.", 404);
+    }
+    user2 = await UserService.getUsersByLastfmUsername(user2Username);
+  } catch (e: any) {
+    if (e instanceof TypedError && e instanceof NotFoundError) {
+      // Continue. We'll handle not found below.
+    } else {
+      handleErrorResponse(e, res);
+    }
+  }
+
+  if (user1 && user2) {
+    res.render("taste-comparison", {
+      secondsToTimeFormat: secondsToTimeFormat,
+      user1,
+      user2,
+    });
+  } else {
+    const users = await UserService.getAllUsers();
+    const usersWithLastfmAccounts = users.filter(
+      (user) => !!user.lastfmAccount && !!user.lastfmAccount.username
+    ) as UserWithLastfmAccountAndId[];
+
+    res.render("configure-comparison", {
+      users: sortUsersByLastfmUsername(usersWithLastfmAccounts),
+      user1: user1 ? user1 : undefined,
+      user2: user2 ? user2 : undefined,
+      error: {
+        message:
+          "Choose one of the provided last.fm accounts to compare. The ability to add new accounts is coming soon.",
+      },
+    });
+  }
 });
 
 indexRouter.get("/debug", async (req: Request, res: Response) => {
@@ -40,3 +116,21 @@ indexRouter.get("/debug", async (req: Request, res: Response) => {
     },
   });
 });
+
+function sortUsersByLastfmUsername(users: UserWithLastfmAccountAndId[]) {
+  return users.sort((a, b) => {
+    if (
+      (a.lastfmAccount.username?.toLocaleLowerCase() as string) <
+      (b.lastfmAccount.username?.toLocaleLowerCase() as string)
+    ) {
+      return -1;
+    }
+    if (
+      (a.lastfmAccount.username?.toLocaleLowerCase() as string) >
+      (b.lastfmAccount.username?.toLocaleLowerCase() as string)
+    ) {
+      return 1;
+    }
+    return 0;
+  });
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,6 +15,13 @@ export async function testEndpoint(): Promise<string> {
   return "Users Service is Online";
 }
 
+/**
+ * Retrieves a user by their ID.
+ *
+ * @param {number} userId - The ID of the user to retrieve.
+ * @returns {Promise<UserWithId>} The user with the specified ID.
+ * @throws {TypedError} If the user with the specified ID is not found or if there's an error in the retrieval process.
+ */
 export async function getUserById(userId: number): Promise<UserWithId> {
   try {
     const user = await prisma.user.findFirst({
@@ -38,6 +45,14 @@ export async function getUserById(userId: number): Promise<UserWithId> {
   }
 }
 
+/**
+ * Creates a new user by their Last.fm username, if a user with that username doesn't already exist.
+ * If a user with that username already exists, the existing user is returned.
+ *
+ * @param {string} username - The Last.fm username of the user to create.
+ * @returns {Promise<UserWithId>} The newly created user.
+ * @throws {TypedError} If the user with the specified Last.fm username already exists or if there's an error in the creation process.
+ */
 export async function createUserByLastfmUsername(
   lastfmUsername: string
 ): Promise<UserWithLastfmAccountAndId> {
@@ -79,6 +94,43 @@ export async function createUserByLastfmUsername(
   }
 }
 
+/**
+ * Retrieves a User by their Last.fm username.
+ *
+ * @param {string} username - The Last.fm username of the user to retrieve.
+ * @returns {Promise<UserWithId>} The user with the specified Last.fm username.
+ * @throws {TypedError<404>} If the user with the specified Last.fm username is not found.
+ * @throws {TypedError<500>} If there's an error in the retrieval process.
+ */
+export async function getUsersByLastfmUsername(
+  username: string
+): Promise<UserWithLastfmAccountAndId> {
+  const user = await prisma.user.findFirst({
+    where: { lastfmAccount: { username } },
+    include: {
+      lastfmAccount: true,
+    },
+  });
+
+  if (!user) {
+    throw TypedError.create(`User with username ${username} not found.`, 404);
+  }
+
+  return createUserFromPrisma(
+    user,
+    user.lastfmAccount
+  ) as UserWithLastfmAccountAndId;
+}
+
+/**
+ * Stores a new user in the database.
+ *
+ *
+ * @param {UserWithId} user - The user to store.
+ * @returns {Promise<UserWithId>} The stored user.
+ * @throws {TypedError<400>} If the user with the specified Last.fm username already exists.
+ * @throws {TypedError<500>} If there's an error in the storage process.
+ */
 export async function storeUser(user: User): Promise<UserWithId> {
   try {
     const checkUserResponse = await prisma.lastfmAccount.findFirst({
@@ -109,7 +161,13 @@ export async function storeUser(user: User): Promise<UserWithId> {
   }
 }
 
-export async function getAllUsers(): Promise<User[]> {
+/**
+ * Retrieves all users from the database.
+ *
+ * @returns {Promise<UserWithId[]>} An array of all users.
+ * @throws {TypedError<500>} If there's an error in the retrieval process.
+ */
+export async function getAllUsers(): Promise<UserWithId[]> {
   try {
     // get user info from database
     const users = await prisma.user.findMany({
@@ -128,6 +186,14 @@ export async function getAllUsers(): Promise<User[]> {
   }
 }
 
+/**
+ * Triggers an update of a user's listen history.
+ *
+ * @param {number} userId - The ID of the user whose listen history should be updated.
+ * @returns {Promise<{BatchUpdateResponse}>} Resolves when the update has been triggered. It provides the status and the number of listens that will be imported.
+ * @throws {TypedError<404>} If the user with the specified ID is not found.
+ * @throws {TypedError<500>} If there's an error in the update process.
+ */
 export async function triggerUpdateListenHistoryByUserId(userId: number) {
   const user = await getUserById(userId);
 
